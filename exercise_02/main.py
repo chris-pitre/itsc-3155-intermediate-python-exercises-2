4055f8df48b9f858a3352e8fc6b22f80e43a0cfa
chris-pitre <c.tophanp3@gmail.com>
2023-01-22 20:05:28 -0500

    Exercise 2 Documentation

--- exercise_02/main.py
+++ exercise_02/main.py
@@ -1,9 +1,26 @@
+# Chris Pitre
+# Intermediate Python Exercises 2
+# Exercise 2
+
 import random
 import time
 
 num = random.randint(15, 35)
 
 def fib(num):
+    """
+    Recursive function that returns the nth number in the fibonacci sequence
+
+    Parameters
+    ----------
+    num : int
+        nth number to be returned in the fibonacci sequence
+    
+    Returns
+    -------
+    int
+        nth number in the fibonacci sequence
+    """
     if num == 0:
         return 0
     elif num == 1 or num == 2:
