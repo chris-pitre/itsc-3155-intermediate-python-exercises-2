aa4c4d4730621fa9febf21344fa522e59631aadc
chris-pitre <c.tophanp3@gmail.com>
2023-01-22 19:44:30 -0500

    Created exercise 3

---
+++ exercise_03/main.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+rng = np.random.default_rng()
+float_array = rng.random(10)
+
+print("Random numbers: ")
+print(float_array)
+
+mean = np.mean(float_array)
+median = np.median(float_array)
+std_dev = np.std(float_array)
+
+print(f"Mean : {mean}, Median: {median}, Standard Deviation: {std_dev}")
